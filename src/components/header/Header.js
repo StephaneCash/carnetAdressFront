cbc512bc6820c1bcf4e77bab39cf136df9778fa4
Software engeneer <kikonistephane@gmail.com>
2023-09-13 18:04:54 +0200

    feat: css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Header.css";
 import logo from "../../assets/images/logo.png";
 import { FiList, FiSearch } from "react-icons/fi"
 
 const Header = () => {
+
+    const [scrollPosition, setPosition] = useState(0);
+
+    useEffect(() => {
+        function updatePosition() {
+            setPosition(window.scrollY);
+        }
+        window.addEventListener('scroll', updatePosition);
+        updatePosition();
+        return () => window.removeEventListener('scroll', updatePosition);
+    }, []);
+
+    useEffect(() => {
+        let headerApp = document.querySelector(".headerApp")
+        if (scrollPosition > 20) {
+            headerApp.classList.add("sticky");
+        } else {
+            headerApp.classList.remove("sticky");
+        }
+    }, [scrollPosition])
+
     return (
         <div className='headerApp'>
             <div className='imageLogo'>
